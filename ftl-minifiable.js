c49036a3f4303c56395e41960ed52803c03d60ce
Nick Muzzin <nick.muzzin@gmail.com>
2019-02-04 23:23:09 -0500

    Now returns precursor stretch distance

--- ftl-minifiable.js
+++ ftl-minifiable.js
@@ -9,8 +9,8 @@
 		}
 		j = c
 		u()
-	},l=v=>{
-		return Math.min(200, Math.max(-200, v))
+	},G=Math,H=G.pow,l=v=>{
+		return G.min(200, G.max(-200, v))
 	},
 		O=document,
 		V=CustomEvent,
@@ -31,13 +31,15 @@
 
 			C = {
 				x: q.x + D.x,
-				y: q.y + D.y
+				y: q.y + D.y,
+				d:H(H(D.x,2)+H(D.y,2),.5)
 			}
 			O[Y](
 				new V("precursormove", {
 					detail: {
 						x: C.x,
-						y: C.y
+						y: C.y,
+						d: C.d
 					}
 				})
 			)
@@ -47,7 +49,7 @@
 		var l = O.elementFromPoint(~~s.x, ~~s.y),Q={
 			bubbles: 1,
 			cancellable: 1
-		},B = A =[],n = f,r='classList',v='prehover';
+		},B = A =[],n = f,r='classList',v='prehover',T=Q;
 		if (f && (!l || l != f)) {
 			f[r].remove(v)
 			f[Y](
@@ -75,8 +77,9 @@
 					n[r].add(v)
 
 			l[r].add(v)
+			T.detail={'d':s.d};
 			l[Y](
-				new V(v, Q)
+				new V(v, T)
 			)
 			f = l
 		}
