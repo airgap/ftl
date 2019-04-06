a83ee6d5ee7a1911c5dca0c91a57e279105c338f
N <nick.muzzin@gmail.com>
2019-04-06 16:42:42 +0200

    Very pretty

--- ftl.js
+++ ftl.js
@@ -1,109 +1,77 @@
-var FTL = {
-	lastPos: {
-		x: 0,
-		y: 0
-	},
-	cpos: {
-		x: 0,
-		y: 0
-	},
-	foreseen: null,
-	moved: e => {
-		var c = {
-			x: e.clientX,
-			y: e.clientY
-		};
-		FTL.cpos = c;
-		FTL.pollCursor();
-	},
-	pollCursor: () => {
-		var c = FTL.cpos;
-
-		//deltas
-		var p = FTL.lastPos || {
-			x: 0,
-			y: 0
-		};
-		var p2 = FTL.cpos || {
-			x: 0,
-			y: 0
-		};
-		var delta = {
-			x: p2.x - p.x,
-			y: p2.y - p.y
-		};
-		if (!delta) delta = {
-			x: 0,
-			y: 0
-		};
-
-		//Keep the cursor from jumping super far away.
-		function lim(v) {
-			return Math.min(200, Math.max(-200, v))
-		}
-		var projectedDelta = {
-			x: lim(delta.x * 4),
-			y: lim(delta.y * 4)
-		};
-		FTL.predictedCpos = {
-			x: c.x + projectedDelta.x,
-			y: c.y + projectedDelta.y
-		};
-		document.dispatchEvent(
-			new CustomEvent("precursormove", {
-				detail: {
-					x: FTL.predictedCpos.x,
-					y: FTL.predictedCpos.y
-				}
-			})
-		);
-		FTL.hoverElement(FTL.predictedCpos);
-		FTL.lastPos = FTL.cpos;
-	},
-	hoverElement: pos => {
-		var el = document.elementFromPoint(~~pos.x, ~~pos.y);
-		if (FTL.foreseen && (!el || el != FTL.foreseen)) {
-			FTL.foreseen.classList.remove("prehover");
-			FTL.foreseen.dispatchEvent(
-				new CustomEvent(
-					"erphover", //The foreseen hover ain't coming through
-					{
-						bubbles: true,
-						cancelable: true
-					}
-				)
-			);
-		}
-		if (el && FTL.foreseen != el) {
-			var oldAncestors = [];
-			var node = FTL.foreseen;
-			while (node) {
-				node = node.parentNode;
-				oldAncestors.push(node);
-			}
-			var newAncestors = [];
-			node = el;
-			while (node) {
-				node = node.parentNode;
-				newAncestors.push(node);
-			}
-			for (var node of Array.from(oldAncestors))
-				if (node && node.classList && newAncestors.indexOf(node) < 0)
-					node.classList.remove("prehover");
-			for (var node of Array.from(newAncestors))
-				if (node && node.classList && oldAncestors.indexOf(node) < 0)
-					node.classList.add("prehover");
-
-			el.classList.add("prehover");
-			el.dispatchEvent(
-				new CustomEvent("prehover", {
-					bubbles: true,
-					cancelable: true
-				})
-			);
-			FTL.foreseen = el;
-		}
-	}
-};
-
-document.addEventListener("mousemove", FTL.moved);
+(k => {
+    var f, j = P = {
+            x: 0,
+            y: 0
+        },
+        M = e => {
+            var c = {
+                x: e.clientX,
+                y: e.clientY
+            };
+            j = c;
+            u()
+        },
+        G = Math,
+        H = G.pow,
+        l = v => {
+            return G.min(200, G.max(-200, v))
+        },
+        O = document,
+        V = (a, b, c) => {
+            a.dispatchEvent(new CustomEvent(b, {
+                detail: c
+            }))
+        },
+        F = 'parentNode',
+        u = k => {
+            var q = j,
+                D = {
+                    x: l((q.x - P.x) * 4),
+                    y: l((q.y - P.y) * 4)
+                },
+                C = {
+                    x: q.x + D.x,
+                    y: q.y + D.y,
+                    d: H(H(D.x, 2) + H(D.y, 2), .5)
+                };
+            V(O, "precursormove", {
+                x: C.x,
+                y: C.y,
+                d: C.d
+            });
+            E(C);
+            P = j
+        },
+        E = s => {
+            var l = O.elementFromPoint(s.x, s.y),
+                B = A = [],
+                n = f,
+                r = 'classList',
+                v = 'prehover';
+            if (f && (!l || l != f)) {
+                f[r].remove(v);
+                V(f, "erphover")
+            }
+            if (l && f != l) {
+                while (n) B.push(n = n[F]);
+                n = l;
+                while (n) A.push(n = n[F]);
+                for (n of B) {
+                    if (n && A.indexOf(n) < 0) {
+                        n[r].remove(v)
+                    }
+                }
+                for (n of A) {
+                    if (n && B.indexOf(n) < 0) {
+                        n[r].add(v)
+                    }
+                }
+                l[r].add(v);
+                V(l, v, {
+                    d: s.d
+                });
+                f = l
+            }
+        };
+    O.addEventListener("mousemove", M)
+})()
